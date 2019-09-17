3d1b15a624a29de44ef56fe7d4e25ff2d9f3e277
doong-jo <sdong001@gmail.com>
2019-09-17 17:24:53 +0900

    feat: carousel 기본 구성 작성
    - div의 id를 부여받은  후 넘겨받은 데이터를 활용하여 Element를 생성
    - option값을 가지고 carousel 의 동작을 분기할 수 있도록 할 예정
    
    close #6

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import "./styles/normarlize.css";
-
 import "./styles/index.scss";
+
+import { getOriginUrl } from "./util/light-dom.js";
+import { requestServer, watchPageVisibility } from "./util/light-api.js";
 import MainCarousel from "./modules/main-carousel.js";
-import MiniCarousel from "./modules/mini-carousel.js";
+// import MiniCarousel from "./modules/mini-carousel.js";
 
-console.log("I am index");
+(async function makeMainCarousel() {
+    let mainCarouselData;
+    try {
+        mainCarouselData = await requestServer(
+            "GET",
+            {},
+            `${getOriginUrl()}/carousel/main-items`,
+            { "Content-Type": "application/json" }
+        );
+    } catch (e) {
+        console.error(e.message);
+        return;
+    }
 
-const mainCarousel = new MainCarousel();
-const miniCarousel = new MiniCarousel();
+    const mainCarousel = new MainCarousel(
+        "#amazon-main-carousel",
+        mainCarouselData
+    );
+})();
---
+++ src/modules/main-carousel-view.js
@@ -0,0 +1,3 @@
+class MainCarouselView {
+    constructor() {}
+}
