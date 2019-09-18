b7ce00d320d23facc7546f69dd1cc7a6f44476e3
doong-jo <sdong001@gmail.com>
2019-09-18 18:14:37 +0900

    refactor: 파일 이름 수정
    - 파일 이름을 수정하고 이에 따라 사용하지 않는 파일 삭제

--- src/modules/main-carousel-view.js
+++
@@ -1,3 +0,0 @@
-class MainCarouselView {
-    constructor() {}
-}
--- src/modules/mini-carousel.js
+++
@@ -1,7 +0,0 @@
-class MiniCarousel {
-    constructor() {
-        console.log("I am MiniCarousel");
-    }
-}
-
-export default MiniCarousel;
