f1fc6d46c4064946d9680b4ab03d3dddb1c94262
doong-jo <sdong001@gmail.com>
2019-09-18 22:13:31 +0900

    feat: header type, url 상수 추가

--- src/util/constants.js
+++ src/util/constants.js
@@ -1,5 +1,15 @@
 const Constants = {
-    EVENT_MOVE_TO_CAROUSEL: "MOVE_TO_CAROUSEL"
+    EVENT_MOVE_TO_CAROUSEL: "MOVE_TO_CAROUSEL",
+
+    METHOD_GET: "GET",
+
+    HEADER_TYPE_JSON: { "Content-Type": "application/json" },
+
+    URL: {
+        MINI_CAROUSEL: "/carousel/mini-carousel",
+        AMAZNON_CAROUSEL: "/carousel/amazon-carousel",
+        CARD: "/carousel/amazon-card"
+    }
 };
 
 export default Constants;
